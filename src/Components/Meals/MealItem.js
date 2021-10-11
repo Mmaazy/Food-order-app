8e184cb57e4a586410183bd1e26f01cadb76466f
Babarwahab <48325299+Babarwahab@users.noreply.github.com>
2021-10-11 17:30:57 +0500

    adding number to cart

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from '../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <>
@@ -15,7 +27,7 @@ const MealItem = (props) => {
         </div>
 
         <div>
-          <MealItemForm />
+          <MealItemForm onAddToCart={addToCartHandler} />
         </div>
       </li>
     </>
